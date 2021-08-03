7676dcd14fe792ba9b20b9c87e296636fe4ff120
n1k0din <nik726@gmail.com>
2021-08-03 17:39:46 +0700

    Add float to int to str example

--- Python/samples3/types/4strings1/1.4.4.py
+++ Python/samples3/types/4strings1/1.4.4.py
@@ -10,3 +10,7 @@ print(int("42") + 1)  # force addition
 print(int("42"), str(42))  # convert from/to string
 
 print("spam" + str(42))  # force concatenation
+
+print(int("5"))
+print(int(5.2))
+print(int("5.2"))  # ValueError
