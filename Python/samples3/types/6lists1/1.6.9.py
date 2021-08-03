73cf12da4ce811693665ec49543f0065145a443c
n1k0din <nik726@gmail.com>
2021-08-03 18:08:34 +0700

    Add .pop() examples

--- Python/samples3/types/6lists1/1.6.9.py
+++ Python/samples3/types/6lists1/1.6.9.py
@@ -5,12 +5,19 @@
 
 li = ["a", "b", "new", "mpilgrim", "z", "example", "new", "two", "elements"]
 
-print(li.remove("z"))
+print(li.remove("z"))  # Does not return a value
 print(li)
 
 li.remove("new")
 print(li)
 
-li.remove("z")
+x = li.pop()
+print(x, li)
 
-print(li)
+x = li.pop(-1)
+print(x, li)
+
+x = li.pop(3)
+print(x, li)
+
+li.remove("z")  # ValueError
