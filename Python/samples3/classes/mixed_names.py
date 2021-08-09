f07973d655d8024451d8b89a60c279859a1ab2bd
n1k0din <nik726@gmail.com>
2021-08-09 11:05:38 +0700

    Fix codestyle; add static method to Fraction

--- Python/samples3/classes/mixed_names.py
+++ Python/samples3/classes/mixed_names.py
@@ -1,12 +1,18 @@
+#!/usr/bin/env python3
+
 class MixedNames:
-  data = "spam"
-  def __init__(self, value):
-    self.data = value
-  def display(self):
-    print((self.data, MixedNames.data))
+    data = "spam"
 
-x = MixedNames(1)
-y = MixedNames(2)
+    def __init__(self, value):
+        self.data = value
 
-x.display()
-y.display()
+    def display(self):
+        print(self.data, MixedNames.data)
+
+
+if __name__ == "__main__":
+    x = MixedNames(1)
+    y = MixedNames(2)
+
+    x.display()
+    y.display()
