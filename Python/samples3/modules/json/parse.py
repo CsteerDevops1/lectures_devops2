f15b1f4599f90ed396b983ca7286101205e2e46a
n1k0din <nik726@gmail.com>
2021-08-09 20:21:52 +0700

    Some codestyle fixes

--- Python/samples3/modules/json/parse.py
+++ Python/samples3/modules/json/parse.py
@@ -1,7 +1,7 @@
 import json
 from pprint import pprint
 
-with open('data.json') as f:
+with open("data.json") as f:
     data = json.load(f)
 
 pprint(data)
@@ -10,8 +10,6 @@ print(data["maps"][0]["id"])
 print(data["masks"]["id"])
 print(data["om_points"])
 
-
-
 data["masks"]["id"] = "vvalore"
-with open('data2.json','w') as f:
-  json.dump(data, f, sort_keys = True, indent = 4)
+with open("data2.json", "w") as f:
+    json.dump(data, f, sort_keys=True, indent=4)
