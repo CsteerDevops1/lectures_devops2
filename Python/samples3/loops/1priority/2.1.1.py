90cbb3b617cf628b255ba3380527a54819d2ed84
Voolodimer <vladimirbruho@gmail.com>
2021-08-13 17:04:00 +0300

    add new table in 2.1.1.py + reference

--- Python/samples3/loops/1priority/2.1.1.py
+++ Python/samples3/loops/1priority/2.1.1.py
@@ -1,32 +1,33 @@
 #!/usr/bin/python3
 """
-Приоритет операторов
+Приоритет операторов\
+https://docs.python.org/3/reference/expressions.html#operator-precedence
 """
 
-#--+--------------------+------------------------
-#  | Operator           |  Description
-#--+--------------------+------------------------
-#   lambda              | Lambda Expression
-#   or                  | Boolean OR
-#   and                 | Boolean AND
-#   not x               | Boolean NOT
-#   in, not in          | Membership tests
-#   is, is not          | Identity tests
-#   <, <=, >, >=, !=, ==| Comparisons
-#   |                   | Bitwise OR
-#   ^                   | Bitwise XOR
-#   &                   | Bitwise AND
-#   <<, >>              | Shifts
-#   +, -                | Addition and subtraction
-#   *, /, %             | Multiplication, Division and Remainder
-#   +x, -x              | Positive, Negative
-#   ~x                  | Bitwise NOT
-#   **                  | Exponentiation
-#   x.attribute         | Attribute reference
-#   x[index]            | Subscription
-#   x[index:index]      | Slicing
-#   f(arguments ...)    | Function call
-#   (expressions, ...)  | Binding or tuple display
-#   [expressions, ...]  | List display
-#   {key:datum, ...}    | Dictionary display
-#   'expressions, ...   | String conversion
+#--+----------------------------------+---------------------------------------+
+#  | Operator                         |  Description                          |
+#--+----------------------------------+---------------------------------------+
+# (expressions...), [expressions...], | Binding or parenthesized expression,  |
+# {key: value...}, {expressions...}   | list display, dictionary display,     |
+#                                     | set display                           |
+# x[index], x[index:index],           | Subscription, slicing,                |
+# x(arguments...), x.attribute        | call, attribute reference             |
+# await x                             | Await expression                      |
+# **                                  | Exponentiation                        |
+# +x, -x, ~x                          | Positive, negative, bitwise NOT       |
+# *, @, /, //, %                      | Multiplication, matrix multiplication,|
+#                                     | division, floor division, remainder 6 |
+# +, -                                | Addition and subtraction              |
+# <<, >>                              | Shifts                                |
+# &                                   | Bitwise AND                           |
+# ^                                   | Bitwise XOR                           |
+# |                                   | Bitwise OR                            |
+# in, not in, is, is not,             | Comparisons, including membership     |
+# <, <=, >, >=, !=, ==                | tests and identity tests              |
+# not x                               | Boolean NOT                           |
+# and                                 | Boolean AND                           |
+# or                                  | Boolean OR                            |
+# if – else                           | Conditional expression                |
+# lambda                              | Lambda expression                     |
+# :=                                  | Assignment expression                 |
+#--+----------------------------------+---------------------------------------+
