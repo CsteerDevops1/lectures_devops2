b39d2f3854cb15b889f00e70c70943fbd753725f
n1k0din <nik726@gmail.com>
2021-08-26 23:08:44 +0700

    Add telnet from samples2

---
+++ Python/samples3/networking/telnet/5.2.py
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+"""
+Получение списка файлов в домашнем каталоге
+"""
+import getpass
+import sys
+import telnetlib
+
+HOST = "localhost" 
+user = raw_input("Enter your remote account: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until("login: ")
+tn.write(user + "\n")
+if password:
+    tn.read_until("Password: ")
+    tn.write(password + "\n")
+
+tn.write("ls\n")
+tn.write("exit\n")
+
+print tn.read_all()
