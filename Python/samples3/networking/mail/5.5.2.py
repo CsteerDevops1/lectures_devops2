34376c3fb8655fd7c20b7333649af0b5c18b9f0e
n1k0din <nik726@gmail.com>
2021-08-26 23:36:14 +0700

    Add mail from samples2

---
+++ Python/samples3/networking/mail/5.5.2.py
@@ -0,0 +1,13 @@
+# -*- encoding: utf-8 -*-
+"""
+Получение почты по протоколу POP3
+"""
+import getpass, poplib
+
+M = poplib.POP3('localhost')
+M.user(getpass.getuser())
+M.pass_(getpass.getpass())
+numMessages = len(M.list()[1])
+for i in range(numMessages):
+    for j in M.retr(i+1)[1]:
+        print j
