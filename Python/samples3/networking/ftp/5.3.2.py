69479101bd1cb5091deaa6ef58ac40576861869f
n1k0din <nik726@gmail.com>
2021-08-26 23:07:20 +0700

    Translate 2to3 ftplib

--- Python/samples3/networking/ftp/5.3.2.py
+++ Python/samples3/networking/ftp/5.3.2.py
@@ -1,10 +1,11 @@
-# -*- encoding: utf-8 -*-
+#!/usr/bin/env python3
 """
 Получение файла с сервера
 """
+from ftplib import FTP
 
-ftp = ftplib.FTP('127.0.0.1', 'book', 'bookpw')
-f = open("MyPycFile.pyc", "wb")
+ftp = FTP('127.0.0.1', 'book', 'bookpw')
+f = open('MyPycFile.pyc', 'wb')
 ftp.set_pasv(1)
 ftp.set_debuglevel(1)
-ftp.retrbinary("RETR AutoIndent.pyc", f.write)
+ftp.retrbinary('RETR AutoIndent.pyc', f.write)
--- Python/samples3/networking/ftp/5.3.3.py
+++ Python/samples3/networking/ftp/5.3.3.py
@@ -1,13 +1,13 @@
-# -*- encoding: utf-8 -*-
+#!/usr/bin/env python3
 """
 Закачка файла на сервер
 """
+from ftplib import FTP
 
-import ftplib
-ftp = ftblib.FTP("ftp.yourServer.com")
-ftp.login("username", "password")
-filename = "index.html" 
-ftp.storlines("STOR " + filename, open(filename))
-filename="app.exe" 
-ftp.storbinary("STOR " + filename, open(filename, "rb"), 1024)
+ftp = FTP('ftp.yourServer.com')
+ftp.login('username', 'password')
+filename = 'index.html'
+ftp.storlines('STOR ' + filename, open(filename))
+filename = 'app.exe'
+ftp.storbinary('STOR ' + filename, open(filename, 'rb'), 1024)
 ftp.quit()
