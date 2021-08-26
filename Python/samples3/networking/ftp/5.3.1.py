be4557957334f5d167ca3a7431d334d4123551c0
n1k0din <nik726@gmail.com>
2021-08-26 23:00:44 +0700

    Add ftp from samples2

---
+++ Python/samples3/networking/ftp/5.3.1.py
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+"""
+Получение списка файлов
+"""
+
+from ftplib import FTP
+ftp = FTP('ftp.cwi.nl')   # connect to host, default port
+ftp.login()               # user anonymous, passwd anonymous@
+ftp.retrlines('LIST')     # list directory contents
